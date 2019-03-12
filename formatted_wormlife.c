8d03a41ebba76011a4fee041bc4920764f82a7b9
Fin <spamsuckersunited@gmail.com>
2019-03-12 16:21:14 -0600

    Add formatted beklemishev worm program

---
+++ formatted_wormlife.c
@@ -0,0 +1,12 @@
+#define K malloc(8)
+ typedef*                                       n;E(n e,n
+ o){n s=            K,t=s;for(               *s=*o;o=o[1];* 
+t=*o)t=t[1         ]=K;t[1]=e;e=             s;}main(c,f,l,j,          a)n*
+    f;{n w=      K,x=w;for(;l=--c;          x=x[1]=K)*x=atoi(f        [c]);for
+     (;w&&++     l;)if(*w){n v=K,z=        v,u=w,t=K  ;for(a=*      v=*w;(u=u
+     [1])&&*     u>=*w;      *z=*u)z      =z[1]=K;     for(x=v[    1],v=K,*v=
+      a-1,1[u=v]=x;u;u=         u[1])w=w[1];for(j=~    l;j++;)u=t=E(t,v);for
+       (;(u=u[1])&&(x=u         [1])&&x[1];);u[1]       =0;w=w?E(w,t):t;}
+        else w=w[1];/*            */printf("%d",         --l);}/*Finian
+         Beklemishev                Rewriting
+           Worms*/
