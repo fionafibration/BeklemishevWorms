55a22bfc226c1e1b93d80a9578abfdc4e40aae79
Fin <spamsuckersunited@gmail.com>
2019-03-12 19:51:20 -0600

    Change worm

--- formatted_wormlife.c
+++ formatted_wormlife.c
@@ -1,4 +1,5 @@
 #define K malloc(8)
+
  typedef*                                       n;E(n e,n
  o){n s=            K,t=s;for(               *s=*o;o=o[1];* 
 t=*o)t=t[1         ]=K;t[1]=e;e=             s;}main(c,f,l,j,          a)n*
@@ -7,6 +8,6 @@ t=*o)t=t[1         ]=K;t[1]=e;e=             s;}main(c,f,l,j,          a)n*
      [1])&&*     u>=*w;      *z=*u)z      =z[1]=K;     for(x=v[    1],v=K,*v=
       a-1,1[u=v]=x;u;u=         u[1])w=w[1];for(j=~    l;j++;)u=t=E(t,v);for
        (;(u=u[1])&&(x=u         [1])&&x[1];);u[1]       =0;w=w?E(w,t):t;}
-        else w=w[1];/*            */printf("%d",         --l);}/*Finian
+        else w=w[1];/*            */printf("%d",         --l);}/*-IOCCC
          Beklemishev                Rewriting
            Worms*/
